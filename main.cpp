ca20c6736c4b0de3df0d6714521586545a7b704a
rlt3 <leroy@bythehilt.com>
2019-02-22 16:09:02 -0500

    refactoring assembly section into a function accepting arguments

--- main.cpp
+++ main.cpp
@@ -34,9 +34,14 @@ class Instruction {
 class Program {
 };
 
-static const unsigned int num_bytes = 4096;
+/* number of bytes allocated for the executable memory */
+static const unsigned int virt_bytes = 4096;
+/* location of executable memory */
 static void *virt = NULL;
-unsigned char *byte = NULL;
+/* byte addressable executable memory */
+static unsigned char *byte = NULL;
+/* entry function into the assembly */
+typedef void (*assembly) (void *addr);
 
 /* 
  * Write a nop slide to safely exit the assembly section to start handling the
@@ -60,7 +65,7 @@ segfault_handler (int sig, siginfo_t *info, void *u)
         return;
 
     /* TODO: doing IO in signal handlers is bad, but ... */
-    if (fault_addr < assem_addr || fault_addr > assem_addr + num_bytes) {
+    if (fault_addr < assem_addr || fault_addr > assem_addr + virt_bytes) {
         printf("Segfault\n");
         exit(1);
     }
@@ -68,69 +73,46 @@ segfault_handler (int sig, siginfo_t *info, void *u)
     printf("caught segfault at %llx. bad memory address %p. virt %llx\n", 
             fault_addr, info->si_call_addr, assem_addr);
     /* write nops for the entire buffer */
-    memset(virt, 0x90, num_bytes);
+    memset(virt, 0x90, virt_bytes);
     /* ret instruction for last byte */
-    byte[num_bytes-1] = 0xc3;
+    byte[virt_bytes-1] = 0xc3;
 }
-
-typedef void (*assembly) (void);
 
 int main(int argc, char *argv[])
 {
-   int value = 42;
-   unsigned long addr = (unsigned long) &value;
+    struct sigaction sa;
+    int value = 42;
+    assembly func;
 
-   virt = mmap(NULL, num_bytes,
+    virt = mmap(NULL, virt_bytes,
                 PROT_READ | PROT_WRITE | PROT_EXEC,
                 MAP_ANONYMOUS | MAP_PRIVATE,
                 0, 0);
-   byte = (unsigned char *) virt;
+    byte = (unsigned char *) virt;
+    func = (assembly) virt;
 
-   /* Catch segfault signal */
-    struct sigaction sa;
+    /* Catch segfault signal */
     memset(&sa, 0, sizeof(struct sigaction));
     sigemptyset(&sa.sa_mask);
     sa.sa_sigaction = segfault_handler;
     sa.sa_flags = SA_SIGINFO;
     sigaction(SIGSEGV, &sa, NULL);
 
-    printf("virt = %p, addr = 0x%lx\n", virt, addr);
-
     /*
-     * movl    $1337, %eax
-     * movl    %eax, <addr>
-     * ret
+     *  movq    $1337, %(rdi)
+     *  ret
      */
-    byte[0] = 0xb8;
-    byte[1] = 0x39;
-    byte[2] = 0x05;
-    byte[3] = 0x00;
-    byte[4] = 0x00;
-    byte[5] = 0xa3;
-    for (unsigned i = 0; i < sizeof(addr); i++) {
-       byte[i+6] = 0xff; /* write a bad address to write to */
-       //byte[i+6] = (addr >> (8 * i)) & 0xff;
-    }
-    byte[14] = 0xc3;
-
-    for (int i = 0; i < 15; i++)
-        printf("%2x ", byte[i]);
-    printf("\n");
-
-    /* 
-     * mov $1337, %eax
-     * ret
-     */
-    //c[0] = 0xB8;
-    //c[1] = 0x39;
-    //c[2] = 0x05;
-    //c[3] = 0x00;
-    //c[4] = 0x00;
-    //c[5] = 0xC3;
+    byte[0] = 0x48;
+    byte[1] = 0xc7;
+    byte[2] = 0x07;
+    byte[3] = 0x39;
+    byte[4] = 0x05;
+    byte[5] = 0x00;
+    byte[6] = 0x00;
+    byte[7] = 0xc3;
 
     printf("before: %d\n", value);
-    assembly func = (assembly) virt;
-    func();
+    func(&value);
     printf("after: %d\n", value);
 
     /* test segfault */
