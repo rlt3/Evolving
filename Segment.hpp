8fd5636623f47826d8833a998d211863b557e70e
rlt3 <leroy@bythehilt.com>
2019-02-23 12:57:13 -0500

    Refactoring exectuable memory handling into its own class. Catching the illegal instruction signal doesn't appear to be portable though ...

---
+++ Segment.hpp
@@ -0,0 +1,145 @@
+#pragma once
+
+#include <cstdlib>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <ucontext.h>
+#include <stdint.h>
+
+/* entry function into the assembly */
+typedef void (*assembly) (void *addr);
+typedef void (*segment_sig_handler) (int, siginfo_t*, void*);
+
+/*
+ * Create and manage a memory segment that is both executable and writable with
+ * signal handlers to prevent the segment from crashing the program.
+ */
+class Segment {
+public:
+    /* 
+     * Starting and ending addresses of currently executing segment. This allow
+     * for signal handling multiple executable segment objects, but only in a
+     * single-threaded environment. Would need to upgrade this data-structure
+     * to allow for multiple threads with signal handling.
+     */
+    static uint64_t addr_beg;
+    static uint64_t addr_end;
+
+    Segment ()
+        : seg_len(getpagesize())
+    {
+        seg = mmap(NULL, seg_len,
+                    PROT_READ | PROT_WRITE | PROT_EXEC,
+                    MAP_ANONYMOUS | MAP_PRIVATE,
+                    0, 0);
+        if (!seg) {
+            perror(strerror(errno));
+            exit(1);
+        }
+    }
+
+    /* Reset the signal handler to its default actions */
+    ~Segment ()
+    {
+        Segment::set_signal_handler((void*) SIG_DFL);
+    }
+
+    /* Set memory segment to WRITE and write to it, clearing it first */
+    void
+    write (uint8_t *bytes, uint32_t length)
+    {
+        Segment::set_prot(seg, seg_len, PROT_WRITE);
+        memset(seg, 0, this->seg_len);
+        memcpy(seg, bytes, length);
+    }
+
+    /* Set memory segment to EXEC and execute */
+    void
+    exec (void *data)
+    {
+        Segment::set_prot(seg, seg_len, PROT_EXEC);
+        Segment::addr_beg = (uint64_t) seg;
+        Segment::addr_end = addr_beg + seg_len;
+        assembly func = (assembly) seg;
+        func(data);
+        Segment::addr_beg = 0;
+        Segment::addr_end = 0;
+    }
+
+    /* Write a nop slide to safely exit the malformed assembly section */
+    static void
+    signal_handler (int sig, siginfo_t *info, void *u)
+    {
+        ucontext_t *context = (ucontext_t*) u;
+        /* 
+         * struct mcontext_t is machine dependent and opaque on purpose. It is
+         * used for restoring the context and we could increment the REG_RIP
+         * value and that would skip to the next instruction (but that could be
+         * bad). We instead use it to know if the faulting instruction is in
+         * the expected location.
+         */
+        uint64_t fault_addr = context->uc_mcontext.gregs[REG_RIP];
+        uint32_t len = Segment::addr_end - Segment::addr_beg;
+        uint8_t *seg = (uint8_t *) Segment::addr_beg;
+
+        if (!(sig == SIGSEGV || sig == SIGILL)) {
+            printf("GOT: %d\n", sig);
+            return;
+        }
+
+        /* 
+         * If the segfault is a real one (not caused by our mutation memory
+         * segment) then remove the handler and return, leting the program
+         * instruction raise the signal again naturally.
+         */
+        if (fault_addr < Segment::addr_beg || fault_addr > Segment::addr_end) {
+            Segment::set_signal_handler((void*) SIG_DFL);
+            return;
+        }
+
+        printf("caught signal %d at %p. bad memory address %p. virt %p\n", 
+                sig, (void*) fault_addr, info->si_call_addr, (void*) Segment::addr_beg);
+
+        Segment::set_prot(seg, len, PROT_WRITE);
+        /* write nops for the entire buffer */
+        memset(seg, 0x90, len);
+        /* ret instruction for last byte */
+        seg[len - 1] = 0xc3;
+        Segment::set_prot(seg, len, PROT_EXEC);
+    }
+
+protected:
+
+    static void
+    set_prot (void* seg, const uint32_t len, const uint32_t protections)
+    {
+        if (mprotect(seg, len, protections | PROT_READ) < 0) {
+            perror(strerror(errno));
+            exit(1);
+        }
+    }
+
+    static void
+    set_signal_handler (void *handler)
+    {
+        /* Catch segfault and sigill signals */
+        struct sigaction sa;
+        memset(&sa, 0, sizeof(struct sigaction));
+        sigemptyset(&sa.sa_mask);
+        sa.sa_sigaction = (segment_sig_handler) handler;
+        sa.sa_flags = SA_SIGINFO;
+        sigaction(SIGSEGV, &sa, NULL);
+        sigaction(SIGILL, &sa, NULL);
+    }
+
+    /* number of bytes allocated for the executable memory */
+    uint32_t seg_len;
+    /* location of executable memory */
+    void *seg;
+};
+
+uint64_t Segment::addr_beg = 0;
+uint64_t Segment::addr_end = 0;
--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
-#include <cstdlib>
 #include <cstdio>
-#include <errno.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <signal.h>
-#include <ucontext.h>
+#include "Segment.hpp"
 
 enum INST {
     MOV,
@@ -35,97 +30,17 @@ class Instruction {
 class Program {
 };
 
-/* number of bytes allocated for the executable memory */
-static const unsigned int virt_bytes = 4096;
-/* location of executable memory */
-static void *virt = NULL;
-/* byte addressable executable memory */
-static unsigned char *byte = NULL;
-/* entry function into the assembly */
-typedef void (*assembly) (void *addr);
-
-void
-virt_exec ()
-{
-    if (mprotect(virt, virt_bytes, PROT_READ | PROT_EXEC) < 0) {
-        perror(strerror(errno));
-        exit(1);
-    }
-}
-
-void
-virt_write ()
-{
-    if (mprotect(virt, virt_bytes, PROT_READ | PROT_WRITE) < 0) {
-        perror(strerror(errno));
-        exit(1);
-    }
-}
-
-/* 
- * Write a nop slide to safely exit the assembly section to start handling the
- * next Program generated.
- */
-static void
-segfault_handler (int sig, siginfo_t *info, void *u)
-{
-    struct ucontext_t *context = (struct ucontext_t*) u;
-    /* 
-     * struct mcontext_t is machine dependent and opaque on purpose. It is used
-     * for restoring the context and we could increment the REG_RIP value and
-     * that would skip to the next instruction (but that could be bad). We 
-     * instead use it to know if the faulting instruction is in the expected
-     * location.
-     */
-    long long fault_addr = context->uc_mcontext.gregs[REG_RIP];
-    long long assem_addr = (long long) virt;
-
-    if (!(sig == SIGSEGV || sig == SIGILL))
-        return;
-
-    /* TODO: doing IO in signal handlers is bad, but ... */
-    if (fault_addr < assem_addr || fault_addr > assem_addr + virt_bytes) {
-        printf("Segfault\n");
-        exit(1);
-    }
-
-    printf("caught signal %d at %llx. bad memory address %p. virt %llx\n", 
-            sig, fault_addr, info->si_call_addr, assem_addr);
-
-    virt_write();
-    /* write nops for the entire buffer */
-    memset(virt, 0x90, virt_bytes);
-    /* ret instruction for last byte */
-    byte[virt_bytes-1] = 0xc3;
-    virt_exec();
-}
-
 int main(int argc, char *argv[])
 {
-    struct sigaction sa;
+    Segment mem;
+    uint8_t byte[4096];
     int value = 42;
-    assembly func;
-
-    virt = mmap(NULL, virt_bytes,
-                PROT_READ | PROT_WRITE | PROT_EXEC,
-                MAP_ANONYMOUS | MAP_PRIVATE,
-                0, 0);
-    byte = (unsigned char *) virt;
-    func = (assembly) virt;
-
-    /* Catch segfault and sigill signals */
-    memset(&sa, 0, sizeof(struct sigaction));
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = segfault_handler;
-    sa.sa_flags = SA_SIGINFO;
-    sigaction(SIGSEGV, &sa, NULL);
-    sigaction(SIGILL, &sa, NULL);
 
     /*
      *  movq    $1337, %(rdi)
      *  ret
      */
-    byte[0] = 0x48;
+    byte[0] = 0x88;
     byte[1] = 0xc7;
     byte[2] = 0x07;
     byte[3] = 0x39;
@@ -134,14 +49,15 @@ int main(int argc, char *argv[])
     byte[6] = 0x00;
     byte[7] = 0xc3;
 
+    mem.write(byte, 8);
+
     printf("before: %d\n", value);
-    virt_exec();
-    func(&value);
+    mem.exec(&value);
     printf("after: %d\n", value);
 
     /* test segfault */
-    char *b = NULL;
-    *b = 5;
+    //char *b = NULL;
+    //*b = 5;
 
     return 0;
 }
