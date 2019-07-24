e3d267ebdf0b951a5ef179175aea364892680cd8
rlt3 <leroy@bythehilt.com>
2019-07-24 00:13:49 +0000

    merging the two programs into one

--- makefile
+++ makefile
@@ -1,7 +1,5 @@
-all:
+all: protected
+evo:
 	g++ -Wall -Werror -o evo main.cpp
-test: protected encrypt
 protected: protected.c
 	gcc -Wall -Werror -o protected protected.c
-encrypt: encrypt.c
-	gcc -Wall -Werror -o encrypt encrypt.c
--- protected.c
+++ protected.c
@@ -1,83 +1,195 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <errno.h>
-#include <signal.h>
-#include <assert.h>
-#include <unistd.h>
-#include <ucontext.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 void
-landing ()
+error (const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+    putchar('\n');
+    exit(1);
+}
+
+void
+read_key (const char *str, uint32_t *key)
+{
+    if (sscanf(str, "0x%x", key) < 1)
+        error("Argument `%s' needs to be a hexadecimal!", str);
+}
+
+void
+protected ()
 {
     /* 
      * Using a string literal here like s = "foobar" would cause the compiler
-     * to put the string "foobar" into a different section of memory. But we
-     * want to encrypt everything inside this area. So instead we construct the
-     * string as an array of bytes so the compiler cannot place it into a
-     * different section.
+     * to put the string "foobar" into a different section of memory (the data
+     * section). But we want to encrypt everything inside this area. So instead
+     * we construct the string as an array of bytes so the compiler cannot
+     * place these bytes it into a section that won't be encrypted.
      */
-    char s[] = {'h', 'e', 'l', 'l', 'o', '\0'};
+    char s[] = {'s', '3', 'c', 'r', '3', 't', '\0'};
     char format[] = {'%', 's', '\n', '\0'};
     printf(format, s);
 }
 
 void
-landing_end ()
+protected_end ()
 {
     /* 
-     * Meant to be empty -- simply marks the end of the landing section so that
-     * we can properly get the correct length of the landing section.
+     * Meant to be empty -- simply marks the end of the protected section so
+     * that we can properly get the correct length of the protected section.
      */
 }
 
+/*
+ * Check of the protected function has been protected yet or not.
+ */
+bool
+is_protected ()
+{
+    /* 
+     * Every function starts with "push %rbp; mov %rsp, %rbp". Knowing this
+     * we can make a very simple check to see if the bytes have either already
+     * been encrypted or if they were decrypted successfully.
+     */
+    uint8_t header[] = { 0x55, 0x48, 0x89, 0xe5 };
+    uint8_t *bytes = (void*) protected;
+    for (int i = 0; i < sizeof(header); i++)
+        if (bytes[i] != header[i])
+            return true;
+    return false;
+}
+/*
+ * Performs bit-arithmetic with the size of the page to clear the least
+ * significant bits of the pointer to find the nearest start of the page.
+ */
 void*
 getpage (void *p, uint64_t size)
 {
     return (void*)((uint64_t)p & ~(size - 1));
 }
 
+/*
+ * Mark the protected function as writeable.
+ */
+void
+mark_protected_writable ()
+{
+    /*
+     * The mprotect system call *must* be called on an address that is page
+     * aligned and *must* be the length of the entire page.  This allows us to
+     * write to this program's instructions which are normally read/execute
+     * only.
+     */
+    uint64_t pagesize = sysconf(_SC_PAGE_SIZE);
+    void* page = getpage(protected, pagesize);
+    if (mprotect(page, pagesize, PROT_WRITE | PROT_EXEC | PROT_READ) < 0)
+        error("mprotect: %s", strerror(errno));
+}
+
+/*
+ * After protected is writable, this xor's each byte of the protected function
+ * with the given key.
+ */
+void
+xor_protected (uint32_t key)
+{
+    uint64_t len = protected_end - protected;
+    uint8_t *bytes = (void*) protected;
+    for (int i = 0; i < len; i++)
+        bytes[i] ^= key;
+}
+
+/*
+ * Get how many bytes into the executable file the protected function should be.
+ */
+uint64_t
+get_protected_offset ()
+{
+    uint64_t pagesize = sysconf(_SC_PAGE_SIZE);
+    void* page = getpage(protected, pagesize);
+    return (void*)protected - page;
+}
+
+/*
+ * Copy the file to a new file of the same name and return the file descriptor.
+ */
+FILE*
+copy_and_open (const char *filename)
+{
+    /*
+     * We do this because opening a file for writing while it is currently
+     * being executed causes the open system call to return the ETXTBSY or
+     * "text busy" error. The kernel has the inode of the file mmap'd in memory
+     * so it can't be modified that way.  So, we sidestep that limitation by
+     * saving all the bytes of the executable file, unlinking the file to that
+     * particular inode, and recreating it under a different inode.
+     */
+    FILE *f = fopen(filename, "r");
+    if (!f)
+        error("%s: %s", filename, strerror(errno));
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    rewind(f);
+    unsigned char bytes[size];
+    fread(bytes, size, 1, f);
+    fclose(f);
+
+    unlink(filename);
+    f = fopen(filename, "w+");
+    if (!f)
+        error("%s: %s", filename, strerror(errno));
+    fwrite(bytes, size, 1, f);
+    rewind(f);
+    chmod(filename, 0775);
+
+    return f;
+}
+
 int
 main (int argc, char **argv)
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <key>\n", argv[0]);
-        exit(1);
-    }
-
     uint32_t key = 0;
-    if (sscanf(argv[1], "0x%x", &key) < 1) {
-        fprintf(stderr, "Argument `%s' needs to be a hexadecimal!\n", argv[1]);
-        exit(1);
+
+    if (argc < 2)
+        error("Usage: %s [encrypt] <key>", argv[0]);
+
+    if (strcmp(argv[1], "encrypt") == 0) {
+        if (is_protected())
+            error("Already encrypted.");
+        /* Encrypt the bytes using the given key */
+        read_key(argv[2], &key);
+        mark_protected_writable();
+        xor_protected(key);
+        /* Write those bytes to a new executable file */
+        FILE *f = copy_and_open(argv[0]);
+        fseek(f, get_protected_offset(), SEEK_CUR);
+        fwrite(protected, protected_end - protected, 1, f);
+        fclose(f);
+        printf("Encrypted.\n");
+    } 
+    else {
+        if (!is_protected())
+            error("Needs to be encrypted first.");
+        /* Decrypt the bytes using the given key */
+        read_key(argv[1], &key);
+        mark_protected_writable();
+        xor_protected(key);
+        /* If it is still protected exit early, otherwise run it */
+        if (is_protected())
+            error("Protected.");
+        protected();
     }
 
-    /*
-     * The mprotect system call *must* be called on an address that is page
-     * aligned and *must* be the length of the entire page. The `getpage`
-     * function simply uses bit-arithmetic to clear the least significant bits
-     * for the size of a page. This allows us to write to this program's
-     * instructions which are normally read/execute only.
-     */
-    uint64_t pagesize = sysconf(_SC_PAGE_SIZE);
-    void* page = getpage(landing, pagesize);
-    if (mprotect(page, pagesize, PROT_WRITE | PROT_EXEC | PROT_READ) < 0) {
-        perror(strerror(errno));
-        exit(1);
-    }
-
-    /*
-     * Read from the landing the encrypted bytes, decrypt them using the
-     * given key, and write them back to the landing.
-     */
-    uint64_t payload_len = landing_end - landing;
-    uint8_t *payload = (void*) landing;
-    for (int i = 0; i < payload_len; i++)
-        payload[i] ^= key;
-    memcpy(landing, payload, payload_len);
-
-    /* Finally, run the protected segment */
-    landing();
     return 0;
 }
